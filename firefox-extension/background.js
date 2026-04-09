fbe03633ae3ccb4bb85e47fe886666a7b99f3673
Skiro1 <135732349+Skiro1@users.noreply.github.com>
2026-04-10 02:03:21 +0700

    Delete firefox-extension directory

--- firefox-extension/background.js
+++
@@ -1,6 +0,0 @@
-// background.js
-if (typeof chrome !== 'undefined' && chrome.runtime && chrome.runtime.onInstalled) {
-    chrome.runtime.onInstalled.addListener(function(details) {
-        if (details.reason === 'install') { chrome.tabs.create({ url: chrome.runtime.getURL('LLM_Compare.html') }); }
-    });
-}
--- firefox-extension/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('openApp').addEventListener('click', () => {
-        chrome.tabs.create({ url: chrome.runtime.getURL('LLM_Compare.html') });
-    });
-
-    document.getElementById('openDocs').addEventListener('click', () => {
-        chrome.tabs.create({ url: chrome.runtime.getURL('api-docs.html') });
-    });
-});
